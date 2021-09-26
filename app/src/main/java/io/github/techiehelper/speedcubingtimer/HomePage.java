28604bdd60439e9f101595874c5266047017d898
TechieHelper <oliverlford@icloud.com>
2021-09-26 10:36:53 +0100

    Initial Commit

---
+++ app/src/main/java/io/github/techiehelper/speedcubingtimer/HomePage.java
@@ -0,0 +1,34 @@
+package io.github.techiehelper.speedcubingtimer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class HomePage extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.page_home, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.timerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(HomePage.this)
+                        .navigate(R.id.action_HomePage_to_TimerFragment);
+            }
+        });
+    }
+}
